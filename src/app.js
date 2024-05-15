bef1e4497693b65f094ff3f3d5e5fb4fc44ad30a
rubenmp92 <97123438+rubenmp92@users.noreply.github.com>
2024-05-15 21:08:55 +0000

    proyecto terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card .number").innerText = generateRandomNumber();
 };
+
+let generateRandomNumber = () => {
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+
+  return numbers[indexNumbers];
+};
+
+let generateRandomSuit = () => {
+  let suit = ["diamond", "spade", "heart", "club"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+
+  return suit[indexSuit];
+};
+
+document
+  .getElementById("changeCardButton")
+  .addEventListener("click", function() {
+    let card = document.querySelector(".card");
+    card.classList.remove("diamond", "spade", "heart", "club"); // Elimina cualquier traje existente
+    let newSuit = generateRandomSuit();
+    card.classList.add(newSuit); // Agrega la nueva clase de traje sin el espacio en blanco
+    card.querySelector(".number").innerText = generateRandomNumber();
+  });
